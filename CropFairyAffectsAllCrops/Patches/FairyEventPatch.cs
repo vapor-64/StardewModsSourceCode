7eaac47f312c0dd4753b1b6c58a865d2724da04d
vapor64 <vaporsixtyfour@gmail.com>
2026-03-08 04:17:23 +0530

    added crop fairy mod source code

---
+++ CropFairyAffectsAllCrops/Patches/FairyEventPatch.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using StardewValley;
+using StardewValley.Events;
+using StardewValley.TerrainFeatures;
+
+namespace CropFairyAffectsAllCrops.Patches;
+
+[HarmonyPatch(typeof(FairyEvent), nameof(FairyEvent.makeChangesToLocation))]
+internal class FairyEventPatch
+{
+    public static bool Prefix()
+    {
+        if (!Game1.IsMasterGame)
+            return false;
+
+        Farm farm = Game1.getFarm();
+
+        foreach (var pair in farm.terrainFeatures.Pairs)
+        {
+            if (pair.Value is HoeDirt { crop: not null } dirt)
+                dirt.crop.growCompletely();
+        }
+
+        return false;
+    }
+}
